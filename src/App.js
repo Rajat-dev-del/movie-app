7bb42f3f568b1084d39f38473389f9f22c555f1a
rajat <rajat.srivastava@hestabit.in>
2021-03-07 10:10:31 +0530

    package files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/style.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Home from './pages/Home.js';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router >
+       
+         <Route exact path="/" component={Home}/>
+         <Route exact path="/About" component={About}/>
+         <Route exact path="/Contact" component={Contact}/>
+       
+    </Router>
     </div>
   );
 }
